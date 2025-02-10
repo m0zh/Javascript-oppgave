e7a8527936ae25027f2d69a8fc6817655fc41f18
m0zh <m0zh.business@gmail.com>
2025-02-10 14:17:34 +0100

    Initial Commit

---
+++ script.js
@@ -0,0 +1,32 @@
+let string1 = "Morten";
+let string2 = "Bergen";
+let string3 = "Stord";
+
+let integer1 = "35";
+let integer2 = "1989";
+
+let favGames = {
+  1: "Witcher 3",
+  2: "World of warcraft",
+  3: "Elden ring",
+};
+
+let hobbies = {
+  1: "Gaming",
+  2: "Golf",
+  3: "Workout",
+};
+
+let array1 = ["Pære, Appelsin, Eple, Kiwi"];
+
+let array2 = ["Audi, Lamborghini, Ferrari, Porche"];
+
+console.log(string1);
+console.log(string2);
+console.log(string3);
+console.log(integer1);
+console.log(integer2);
+console.log(favGames);
+console.log(hobbies);
+console.log(array1);
+console.log(array2);
